2548203e1f023f9ea55144016332cab12ba8e0fe
tari2025 <denilestari272@gmail.com>
2025-05-07 20:10:39 +0700

    cara cout

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -100,3 +100,10 @@ void binarySearch()
                 low = mid + 1;
             }
         } while (low <= high);
+        cout << "\nMaaf! Elemen " << item << " tidak ditemukan dalam array.\n";
+
+        cout << "\nIngin mencari lagi? (y/n): ";
+        cin >> ulang;
+
+    } while (ulang == 'y' || ulang == 'Y');
+}
