192f9e180058cd9a547d0b7dbf4a9d7b89cdc3b6
tari2025 <denilestari272@gmail.com>
2025-05-07 20:05:49 +0700

    codingan binarySearch

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -26,4 +26,11 @@ void input()
     cout << "Masukkan Elemen Array" << endl;
     cout << "==============================" << endl;
 
-    for (int i = 0; i < n; i
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << (i + 1) << ": ";
+        cin >> elemen[i]
+    }
+}
+
+void bubble
