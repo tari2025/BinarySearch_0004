7c9dd37bf0026c783f641f2f5a4c54ef41f1f67f
tari2025 <denilestari272@gmail.com>
2025-05-07 20:00:33 +0700

    cout

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -9,3 +9,21 @@ void input()
 {
     while (true)
     {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> n;
+
+        if (n <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nJumlah elemen tidak boleh lebih dari 10. Silakan coba lagi.\n";
+        }
+    }
+
+    cout << "==============================" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "==============================" << endl;
+
+    for (int i = 0; i < n; i
